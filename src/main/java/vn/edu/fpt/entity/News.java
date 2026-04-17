46f43497a6e0e15754ca00ee384c885e24731488
unknown <dungpche171832@fpt.edu.vn>
2026-04-17 20:38:57 +0700

    add entity

---
+++ src/main/java/vn/edu/fpt/entity/News.java
@@ -0,0 +1,47 @@
+package vn.edu.fpt.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(
+        name = "news",
+        indexes = {
+                @Index(name = "idx_news_title", columnList = "title"),
+                @Index(name = "idx_news_status", columnList = "status"),
+                @Index(name = "idx_news_created_at", columnList = "create_at")
+        }
+)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class News extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(name = "title", nullable = false, length = 255)
+    private String title;
+
+    @Column(name = "slug", unique = true, length = 255)
+    private String slug;
+
+    @Column(name = "content", columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Column(name = "thumbnail", length = 500)
+    private String thumbnail;
+
+    @Column(name = "summary", length = 500)
+    private String summary;
+
+    @Column(name = "status", nullable = false)
+    private String status;
+    // DRAFT / PUBLISHED / HIDDEN
+
+    @Column(name = "is_featured", nullable = false)
+    private Boolean isFeatured = false;
+}
